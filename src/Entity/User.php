dfab29ae39d0a80e55bef65d8b3d070db4e8637b
Noah Sfez <sfz.noah@gmail.com>
2025-03-21 09:44:43 +0100

    Enhance User entity with serialization groups for API normalization and denormalization

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,8 +7,12 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Attribute\Groups;
 
-#[ApiResource()]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']]
+)]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -16,27 +20,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(groups: 'read')]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(groups: ['read', 'write'])]
     private ?string $email = null;
 
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
+    #[Groups(groups: ['read', 'write'])]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Groups(groups: 'read')]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(groups: ['read', 'write'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(groups: ['read', 'write'])]
     private ?string $lastname = null;
 
     public function getId(): ?int
